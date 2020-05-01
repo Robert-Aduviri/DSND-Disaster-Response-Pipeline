ec1f562392f3bb1ffb01b2cc3cb0c10b29792e19
Robert-Alonso <robert.alonso.ac@gmail.com>
2020-05-01 03:00:48 -0500

    feat: Add logging to ETL

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [Udacity Data Science Nanodegree](https://www.udacity.com/course/data-scientist-nanodegree--nd025) project, covering ETL and Machine Learning pipelines.
 
-# Project Components
+# Project components and instructions
 
 ## 1. ETL Pipeline
 Data cleaning pipeline, in process_data.py:
@@ -11,6 +11,9 @@ Data cleaning pipeline, in process_data.py:
 - Merges the two datasets
 - Cleans the data
 - Stores it in a SQLite database
+
+To run ETL pipeline that cleans data and stores in database
+    `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
 
 ## 2. ML Pipeline
 Machine Learning pipeline, in train_classifier.py:
@@ -22,8 +25,16 @@ Machine Learning pipeline, in train_classifier.py:
 - Outputs results on the test set
 - Exports the final model as a pickle file
 
+To run ML pipeline that trains classifier and stores it
+    `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+
 ## 3. Flask Web App
 We are providing much of the flask web app for you, but feel free to add extra features depending on your knowledge of flask, html, css and javascript. For this part, you'll need to:
 
 - Modify file paths for database and model as needed
 - Add data visualizations using Plotly in the web app. One example is provided for you
+
+Run the following command in the app's directory to run your web app.
+    `python run.py`
+
+Go to http://0.0.0.0:3001/
--- data/process_data.py
+++ data/process_data.py
@@ -1,13 +1,17 @@
+import sys, os, logging
 from typing import Tuple
 import pandas as pd
 from sqlalchemy import create_engine
 
+logger = logging.getLogger(__name__)
 
-def extract(messages_filename: str, categories_filename: str) \
+def extract(messages_filepath: str, categories_filepath: str) \
     -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Read the messages and categories csv files into dataframes"""
-    messages = pd.read_csv(messages_filename)
-    categories = pd.read_csv(categories_filename)
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    logger.info(f'{len(messages)} records found in messages')
+    logger.info(f'{len(messages)} records found in categories')
     return messages, categories
 
 
@@ -43,40 +47,75 @@ def transform_categories(categories: pd.DataFrame) -> pd.DataFrame:
 def transform(messages: pd.DataFrame, categories: pd.DataFrame) \
     -> pd.DataFrame:
     """Transforms and merges the messages and categories dataframes"""
+    logger.info('Cleaning data...')
     messages = transform_messages(messages)
     categories = transform_categories(categories)
     data = pd.merge(messages, categories, on='id')
+    logger.info(f'Joined {len(messages)} unique messages')
     return data
 
 
-def load(data: pd.DataFrame, db_filename: str, table_name: str):
+def load(data: pd.DataFrame, db_filepath: str, table_name: str):
     """Save data in a table inside a SQLite db 
 
     Args:
     data: dataframe
-    db_filename: SQLite database file name
+    db_filepath: SQLite database file name
     table_name: table name inside the SQLite database
     """
-    engine = create_engine(f'sqlite:///{db_filename}')
+    if os.path.exists(db_filepath):
+        os.remove(db_filepath)
+    
+    logger.info(f'Saving data...    DATABASE: {db_filepath}')
+    engine = create_engine(f'sqlite:///{db_filepath}')
     data.to_sql(table_name, engine, index=False)
 
 
-def etl(messages_file: str = 'data/messages.csv', 
-        categories_file: str = 'data/categories.csv',
-        db_file: str = 'DisasterResponse_preprocessed.db') \
+def etl(messages_filepath: str = 'data/messages.csv', 
+        categories_filepath: str = 'data/categories.csv',
+        db_filepath: str = 'data/DisasterResponse_preprocessed.db') \
         -> pd.DataFrame: 
     """Extract, Transform and Load preprocessed data from the csv files
 
     Args:
     messages_file: messages csv file name
     categories_file: categories csv file name
-    db_file: output data SQLite database file name
+    db_filepath: output data SQLite database file name
 
     Returns:
     data: preprocessed data
     """
-    messages, categories = extract(messages_file, categories_file)
+    logger.info(f'Loading data into {db_filepath}...')
+    logger.info(f'MESSAGES: {messages_filepath} | CATEGORIES: {categories_filepath}')
+    messages, categories = extract(messages_filepath, categories_filepath)
     data = transform(messages, categories)
-    load(data, db_file, 'message')
+    load(data, db_filepath, 'message')
     return data
     
+
+def main():
+    if len(sys.argv) == 4:
+        logger.info('Starting ETL...')
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+        etl(messages_filepath, categories_filepath, database_filepath)
+        logger.info('ETL process finished successfully')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    import logging
+
+    logging.basicConfig(
+        filename='data/etl.log',
+        format='%(asctime)s - %(levelname)s - %(message)s', 
+        datefmt='%d-%b-%y %H:%M:%S',
+        level=logging.INFO
+    )
+    main()
